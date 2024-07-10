600556b0c6912729abc39980361400a02ffb9373
stuartt <34777184+RealMCoded@users.noreply.github.com>
2024-07-10 13:38:18 -0400

    added documentation to user.js

--- user.js
+++ user.js
@@ -1,9 +1,18 @@
+/*
+    User installable command example
+    You cannot use SlashCommandBuilder here, you have to type manual JSON data.
+    To help with this, I show the data for each command in the console when you push it.
+    You should be able to take that data and add the following to it at the very end to make it user installable:
+    
+    integration_types: [0, 1], //0=server installable, 1=user installable
+    contexts: [0, 1, 2], //0=GUILD, 1=BOT_DM, 2=PRIVATE_CHANNEL. You should be able to leave this as default.
+*/
 module.exports = {
     data: {
         name: "userappcommand",
         description: "User install command test",
-        integration_types: [1],
-        contexts: [0, 1, 2],
+        integration_types: [0, 1], //0=server installable, 1=user installable
+        contexts: [0, 1, 2], //0=GUILD, 1=BOT_DM, 2=PRIVATE_CHANNEL. You should be able to leave this as default.
         "options": [
             {
                 "name": "animal",
@@ -20,6 +29,6 @@ module.exports = {
         ]
     },
     async execute(interaction){
-        await interaction.reply("hey!")
+        await interaction.reply("This is a user app command!")
     }
 }
