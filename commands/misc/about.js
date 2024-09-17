c31e86d168fed807c6fb660e378c0fc620822330
stuartt <34777184+RealMCoded@users.noreply.github.com>
2024-09-17 14:09:36 -0400

    Update about.js

--- commands/misc/about.js
+++ commands/misc/about.js
@@ -14,10 +14,6 @@ module.exports = {
 			.setDescription("Learn more about what VNCs are"))
 		.addSubcommand(subcommand =>
 			subcommand
-			.setName("honeypot")
-			.setDescription("Learn about Honeypots"))
-		.addSubcommand(subcommand =>
-			subcommand
 			.setName("github")
 			.setDescription("Get a link to the bot's Github (sent ephemerally)")),*/
 	data: {
@@ -35,14 +31,6 @@ module.exports = {
 			name: 'vnc',
 			name_localizations: undefined,
 			description: 'Learn more about what VNCs are',
-			description_localizations: undefined,
-			options: []
-		  },
-		  {
-			type: 1,
-			name: 'honeypot',
-			name_localizations: undefined,
-			description: 'Learn about Honeypots',
 			description_localizations: undefined,
 			options: []
 		  },
@@ -73,21 +61,16 @@ module.exports = {
         if(cmd == "bot"){
             const embed = new EmbedBuilder()
                 .setTitle("About Public VNC Resolver")
-                .setDescription(`Public VNC Resolver is a bot dedicated to showcasing all the insecure VNCs across the world for entertainment reasons.
+                .setDescription(`Public VNC Resolver is a Discord bot dedicated to showcasing all the insecure VNCs across the world, using [ComputerNewb's VNC Resolver](https://computernewb.com/vncresolver/) API.
 				
-				**NOTE:** This bot is for research and entertainment purposes only! Please do not attempt to connect to these VNCs, as you could connect to a Honeypot (</about honeypot:1001927609455738911>).
+				**Note:** This bot is for research and entertainment purposes only. Please do not attempt to connect to these VNCs, as you could connect to a [Honeypot](https://en.wikipedia.org/wiki/Honeypot_(computing)). I am not responsible if anything bad happens.
 				
-				**I am not responsible if anything bad happens!**`)
+				[Terms of Service](https://github.com/RealMCoded/Public-VNC-Bot/blob/master/TERMS-OF-SERVICE.md) - [Privacy Policy](https://github.com/RealMCoded/Public-VNC-Bot/blob/master/PRIVACY-POLICY.md)`)
                 .setThumbnail("https://cdn.discordapp.com/avatars/740290115972235364/88ac1f814a3011a2d56076948cffd9e7.png")
-            await interaction.reply({embeds: [embed]})
-		} else if (cmd == "honeypot"){
-			const embed = new EmbedBuilder()
-                .setTitle("What are Honeypots?")
-                .setDescription("> In computer terminology, a honeypot is a trap set to detect, deflect or in some manner counteract attempts at unauthorized use of information systems. Generally, honeypots essentially turn the tables for Hackers and Computer Security Experts.\n\n*Source: [Honeypot (computing) - Wikipedia](https://en.wikipedia.org/wiki/Honeypot_(computing))*")
             await interaction.reply({embeds: [embed]})
         } else if (cmd == "vnc"){
             const embed = new EmbedBuilder()
-                .setTitle("What is a VNC")
+                .setTitle("What is a VNC?")
                 .setDescription("> In computing, Virtual Network Computing is a graphical desktop-sharing system that uses the Remote Frame Buffer protocol to remotely control another computer. It transmits the keyboard and mouse input from one computer to another, relaying the graphical-screen updates, over a network.\n\n*source: [Virtual Network Computing - Wikipedia](https://en.wikipedia.org/wiki/Virtual_Network_Computing)*")
             await interaction.reply({embeds: [embed]})
         } else if (cmd == "github"){
